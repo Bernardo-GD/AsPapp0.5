28e1103b50a495bdd63c43e66a7c55126067420a
Bernardo-GD <80055739+Bernardo-GD@users.noreply.github.com>
2021-06-21 22:43:24 -0500

    Mod ActCalendario, AmoroIndividual, botones ambos

--- app/src/main/java/com/pachoncito/aspapp/AmorometroIndividual.java
+++ app/src/main/java/com/pachoncito/aspapp/AmorometroIndividual.java
@@ -2,6 +2,8 @@ package com.pachoncito.aspapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class AmorometroIndividual extends AppCompatActivity {
@@ -10,5 +12,14 @@ public class AmorometroIndividual extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amorometro_individual);
+
+        //cambiar el color dde la barra donde está la hora, notificaciones, etc
+        getWindow().setStatusBarColor(Color.parseColor("#01AAFF"));
+
+        //cambiar fondo a blanco
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
+        //quitar actionbar
+        getSupportActionBar().setTitle("Amorometro individual");
+
     }
 }
